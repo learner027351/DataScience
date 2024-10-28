7ea1964b50b54de51ca2e5c33e6031b62f1ddaaa
learner027351 <vineetsingh27351@gmail.com>
2024-10-28 11:34:15 +0530

    Intro Data Science project

---
+++ Quote.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+from time import sleep
+from random import choice
+
+all_quotes=[]
+base_url="http://quotes.toscrape.com/"
+url="/page/1"
+
+while url:
+    res=requests.get(f"{base_url}{url}")
+    print(f"Now Scraping{base_url}{url}")
+    soup=BeautifulSoup(res.text,"html.parser")
+
+    quotes=soup.find_all(class_="quote")
+    for quote in quotes:
+        all_quotes.append({
+            "text":quote.find(class_="text").get_text(),
+            "author":quote.find(class_="author").get_text(),
+            "bio-link":quote.find("a")["href"]
+        })
+    next_btn=soup.find(_class="next")
+    url=next_btn.find("a")["href"] if next_btn else None
+    sleep(2)
+
+quote=choice(all_quotes)
+remaining_guesses=4
+print("here is the quote: ")
+print(quote["text"])
+
+guess=''
+while guess.lower()!= quote["author"].lower() and remaining_guesses>0:
+    guess=input(
+        f"who said this quote? Guesses remaining {remaining_guesses}"
+    )
+    if guess==quote["author"]:
+        print("Congratulations!!! You got it right.")
+        break
+    remaining_guesses-=1
+    
+    if remaining_guesses==3:
+        res=requests.get(f"{base_url}{quote['bio-link']}")
+        soup=BeautifulSoup(res.text,"html.parser")
+        birth_date=soup.find(class_="author-born-Sdate").get_text()
+        birth_place=soup.find(class_="author-born-location").get_text()
+        print(
+            f"Here's s hint: The author was born on {birth_date}{birth_place}"
+        )
+    elif remaining_guesses==2:
+        print(
+            f"Here's s hint:The author's first name starts with:{quote['author'][0]}"
+        )
+    elif remaining_guesses==1:
+        last_initial=quote["author"].split(" ")[1][0]
+        print(
+            f"Here's a hint: The author's last name starts with:{last_initial}"
+        )
+
+    else:
+        print(
+            f"Sorry,you ran ot of guesses.The answer was {quote['author']}"
+        )
+    
